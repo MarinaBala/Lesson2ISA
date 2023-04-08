ba7bef13d703aa86980c4f002b9b8c3dad1cdec0
ilnrdvltshn <ilnurdavletshin.ru@gmail.com>
2023-04-08 13:05:24 +0500

    реализовали функуции в массиве

---
+++ 2isa/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2isa
+{
+    internal class Class1
+    {
+        internal static double Sredne( int[] array) => array.Sum() / array.Length; //МЕТОД ПОИСКА СДРЕДНЕГО ЗНАЧЕНИЯ
+
+
+        internal  static int  Max (int[] array )
+        {
+           int max = array[0];
+
+            for ( int i = 0; i< array.Length; i++)
+            {
+                max = array[i] > max ? array[i] : max;
+            }
+            return max;
+        }
+
+        internal static int PEREVOR(int[] array)
+        {
+            int[] mass=new int[array.Length];
+            int a = array.Length;
+            for (int i=0;array.Length>i;i++)
+            {
+                mass[a] = array[i];
+                Console.WriteLine(mass[i]);
+                a--;
+            }
+            return mass[a];
+        }
+    }
+}
--- 2isa/Program.cs
+++ 2isa/Program.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace _2isa
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            int[] d = new int[5] { 1, 2, 3, 4, 5};
+            Class1.PEREVOR(d);
         }
     }
+
+
 }
